2458e3b6f96f601e7c1b537bccd747d43106f605
Luis Costantin <luisclaudioc@hotmail.com>
2024-04-18 19:11:01 +0100

    first images

---
+++ app.js
@@ -0,0 +1,16 @@
+const projects = [
+    {
+        name: "Calculator",
+        image: "public/images/calculator.png",
+        href: "https://codepen.io/Luis-Costantin/full/ExGEmYJ",
+        skills: ["HTML", "CSS", "JavaScript"],
+    },
+    {
+        name: "Drum Machine",
+        image: "public/images/drum-machine.png",
+        href: "https://codepen.io/Luis-Costantin/full/WNLZJLm",
+        skills: ["JavaScript, React.js"]
+    }
+]
+
+console.log(projects)
